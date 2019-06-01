e9f4cbd492b930a11ccd1f0f06828d28352eaba7
gmbwest <37163042+gmbwest@users.noreply.github.com>
2019-05-31 20:09:26 -0400

    Update and rename lolo.js to gmbplaylist.js

--- js/lolo.js
+++
@@ -1,8 +0,0 @@
-
-	var mediaElements = document.querySelectorAll('video, audio');
-
-	for (var i = 0, total = mediaElements.length; i &lt; total; i++) {
-		new MediaElementPlayer(mediaElements[i], {
-			features: ['prevtrack', 'playpause', 'nexttrack', 'current', 'progress', 'duration', 'volume', 'playlist', 'shuffle', 'loop', 'fullscreen'],
-		});
-	}
